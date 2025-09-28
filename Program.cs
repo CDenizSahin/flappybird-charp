3392065c359119034eb9d73596b89c3524fef3bc
CDenizSahin <cemdeniz921@gmail.com>
2025-09-28 18:13:07 +0300

    Web versiyonu eklendi - Blazor WebAssembly ile cross-platform oyun

--- Program.cs
+++ Program.cs
@@ -1,16 +1,11 @@
-namespace FlappyBirdGame;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using FlappyBirdWeb;
 
-static class Program
-{
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
-    {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
-    }    
-}
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
